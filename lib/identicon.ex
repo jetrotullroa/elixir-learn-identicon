9f1f98f24262c830edd716f0014aff367e804b0a
Jetro Tull Roa <jetrotullroa@gmail.com>
2017-02-20 16:31:03 +0800

    build grid and mirror grid

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -19,6 +19,22 @@ defmodule Identicon do
     |> build_grid
   end
 
+  def build_grid(%Identicon.Image{hex: hex_list} = image) do
+    grid =
+      hex_list
+      |> Enum.chunk(3)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      |> Enum.with_index
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def mirror_row(row) do
+    [first, second | _tail] = row
+    row ++ [second, first]
+  end
+
   def pick_color(%Identicon.Image{hex: [r, g, b | _hex_list_tail]} = image) do
   %Identicon.Image{image | color: {r, g , b} }
   end
